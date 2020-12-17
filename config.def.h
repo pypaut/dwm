7072b8745c78e0ab590d202fc49853e66cfc999f
Geoffrey Jount <geoffrey.jount@epita.fr>
2020-12-17 20:16:55 +0100

    Fonts & colors

--- config.def.h
+++ config.def.h
@@ -8,13 +8,13 @@ static const unsigned int gappx     = 6;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+static const char *fonts[]          = { "Iosevka Nerd Font Mono:style=Medium:size=10" };
+static const char dmenufont[]       = "Iosevka Nerd Font Mono:style=Medium:size=10";
+static const char col_gray1[]       = "#2e3440";  /* tag tabs color */
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#4c566a";  /* bar main color */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
